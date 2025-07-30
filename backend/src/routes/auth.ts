030ed255d2160b3bd49275fc1466bcca31d9bd6a
genius-ayush <ayushsanjayrawal@gmail.com>
2025-07-30 12:57:46 +0530

    finished creating schems

---
+++ backend/src/routes/auth.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+
+const router = Router() ; 
+
+
+router.post("register" , (req , res)=>{
+    
+})
+
+
+router.post("/login" , (req , res)=>{
+
+})
+
+
+router.get("/me" , (req , res)=>{
+
+})
+
+export default router ; 
